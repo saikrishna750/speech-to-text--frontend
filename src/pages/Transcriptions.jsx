411b343967693ddfaeea2d5860880e5484b7515f
saikrishna750 <saikrishnaeligeti56@gmail.com>
2025-08-07 00:05:21 +0530

    added date and time for each transcriptions

--- src/pages/Transcriptions.jsx
+++ src/pages/Transcriptions.jsx
@@ -33,6 +33,28 @@ const Transcriptions = () => {
         setHistory(prev => (prev.filter(each => each.id !== id)))
     }
 
+    const setDataTime = (timeStamp) =>{
+        // const date =  dateObj.getDate()
+        const dateObj = new Date(timeStamp)
+        let date = dateObj.getDate()
+        date = date < 10 ? `0${date}` : date
+        let month = dateObj.getMonth()
+        month = month < 10 ? `0${month}` : month
+        let year = dateObj.getFullYear()
+
+        let hours = dateObj.getHours()
+        hours = hours < 10 ? `0${hours}` : hours 
+        let minutes = dateObj.getMinutes()
+        minutes = minutes < 10 ? `0${minutes}` : minutes
+        let seconds = dateObj.getSeconds()
+        seconds = seconds < 10 ? `0${seconds}` : seconds 
+
+        const showTime = `${date}-${month}-${year} ${hours}:${minutes}:${seconds}`
+        return showTime
+    }
+
+    console.log(history)
+
     return (
         <>
             <div className="bg-gray-950 min-h-screen ">
@@ -44,7 +66,10 @@ const Transcriptions = () => {
                     : (<ul className="text-black pb-10">
                             {history.map((each) => (
                                 <li className ='bg-gray-900 flex flex-row justify-between items-center m-1 mx-3 px-3 py-2 md:mx-18 md:px-10 py-3 border border-gray-600 rounded-sm shadow-sm' key={each.id}> 
-                                    <p className="mr-5 text-neutral-300">{each.transcription}</p> 
+                                    <div>
+                                        <p className="mb-2 text-sm text-gray-500">{setDataTime(each.created_at)}</p>
+                                        <p className="mr-5 text-neutral-300">{each.transcription}</p> 
+                                    </div>
                                     <button onClick={() => onDeleteTranscript(each.id)} className="text white pt-2 self-start">
                                         <MdDelete className="w-[20px] h-[20px] text-white"/>
                                     </button>
